75925986ec300c919568520fbd1b2241c8d8fffc
Nathan McGuirt <nathan@meltphace.org>
2024-11-22 22:44:24 -0500

    Fix bad location constraint

--- storage.tf
+++ storage.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "static-site" {
   name          = "beta.meltphace.org"
-  location      = "us-east-4"
+  location      = "US-EAST4"
   force_destroy = true
 
   uniform_bucket_level_access = true
