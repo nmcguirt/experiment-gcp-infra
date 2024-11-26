f38b79d554ef393dbaff6c7573035288f0824dca
Nathan McGuirt <nathan@meltphace.org>
2024-11-25 22:23:54 -0500

    Fix DNS zone name

--- dns.tf
+++ dns.tf
@@ -1,5 +1,5 @@
 resource "google_dns_managed_zone" "beta-meltphace-org" {
   name        = "beta-meltphace-org"
-  dns_name    = "beta.meltphace.org"
+  dns_name    = "beta.meltphace.org."
   description = "beta DNS zone"
 }
