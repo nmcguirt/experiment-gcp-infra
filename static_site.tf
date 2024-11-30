df825d11bbe3c80d19f1f569454bf124dc242c37
Nathan McGuirt <nathan@meltphace.org>
2024-11-30 13:32:07 -0500

    Delete phase 2

--- static_site.tf
+++ static_site.tf
@@ -2,11 +2,11 @@
 #### DNS Zone and Managed TLS Cert #####
 ########################################
 
-resource "google_dns_managed_zone" "beta-meltphace-org" {
-  name        = "beta-meltphace-org"
-  dns_name    = "beta.meltphace.org."
-  description = "beta DNS zone"
-}
+# resource "google_dns_managed_zone" "beta-meltphace-org" {
+#   name        = "beta-meltphace-org"
+#   dns_name    = "beta.meltphace.org."
+#   description = "beta DNS zone"
+# }
 
 # resource "google_certificate_manager_certificate" "default" {
 #   name        = "dns-cert-${google_dns_managed_zone.beta-meltphace-org.name}"
@@ -60,19 +60,19 @@ resource "google_dns_managed_zone" "beta-meltphace-org" {
 #### Storage and Load Balancer ####
 ###################################
 
-resource "google_storage_bucket" "static-site" {
-  name          = "beta.meltphace.org"
-  location      = "US-EAST4"
-  force_destroy = true
+# resource "google_storage_bucket" "static-site" {
+#   name          = "beta.meltphace.org"
+#   location      = "US-EAST4"
+#   force_destroy = true
 
 
-  uniform_bucket_level_access = true
+#   uniform_bucket_level_access = true
 
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
-}
+#   website {
+#     main_page_suffix = "index.html"
+#     not_found_page   = "404.html"
+#   }
+# }
 
 # # Make bucket public
 # resource "google_storage_bucket_iam_member" "static-site" {
